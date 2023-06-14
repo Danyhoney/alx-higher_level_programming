2df5727e818e934719d6c81f0cb6bea61fb1368a
Danyhoney <danielnyong4faith@gmail.com>
2023-06-14 03:28:29 -0700

    update

--- 0x07-python-test_driven_development/test/0-add_integer.py
+++ 0x07-python-test_driven_development/test/0-add_integer.py
@@ -1,30 +1,95 @@
-#!/usr/bin/python3
-"""
+The ``0-add_integer`` module
+============================
 
-This module is composed by a function that adds two numbers
+Using ``add_integer``
+---------------------
 
-"""
+Importing function from the module:
+    >>> add_integer = __import__('0-add_integer').add_integer
 
+Adding 1 and 2
+    >>> add_integer(1, 2)
+    3
 
-def add_integer(a, b=98):
-    """ Function that adds two integer and/or float numbers
+Subtracting 100 and 2
+    >>> add_integer(100, -2)
+    98
 
-    Args:
-        a: first number
-        b: second number
+Adding 2.1 and 98
+    >>> add_integer(2.1)
+    100
 
-    Returns:
-        The addition of the two given numbers
+Subtracting 100.3 and 2
+	    >>> add_integer(100.3, -2)
+	    98
 
-    Raises:
-        TypeError: If a or b aren't integer and/or float numbers
+Adding an integer number and a string
+    >>> add_integer(4, "School")
+    Traceback (most recent call last):
+	      ...
+    TypeError: b must be an integer
 
-    """
+Passing None
+    >>> add_integer(None)
+    Traceback (most recent call last):
+	      ...
+    TypeError: a must be an integer
 
-    if not isinstance(a, int) and not isinstance(a, float):
-        raise TypeError("a must be an integer")
-    if not isinstance(b, int) and not isinstance(b, float):
-        raise TypeError("b must be an integer")
-    a = int(a)
-    b = int(b)
-    return (a + b)
+Adding a letter and a number
+    >>> add_integer('1', 1)
+    Traceback (most recent call last):
+              ...
+    TypeError: a must be an integer
+
+Adding two letters
+    >>> add_integer('2', '1')
+    Traceback (most recent call last):
+              ...
+    TypeError: a must be an integer
+
+Adding a tuple
+    >>> add_integer((1, 1))
+    Traceback (most recent call last):
+              ...
+    TypeError: a must be an integer
+
+Adding a number and a list
+    >>> add_integer(123, [])
+    Traceback (most recent call last):
+	      ...
+    TypeError: b must be an integer
+
+Passing a string
+    >>> add_integer("Hello")
+    Traceback (most recent call last):
+	      ...
+    TypeError: a must be an integer
+
+Adding two float numbers
+    >>> add_integer(2.9, 2.9)
+    4
+
+Subtracting 98 and 1
+    >>> add_integer(-1)
+    97
+
+Case Overflow:
+
+    >>> add_integer(float('inf'), 0)
+    Traceback (most recent call last):
+    	      ...
+    OverflowError: cannot convert float infinity to integer
+
+Case Overflow 2:
+
+    >>> add_integer(float('inf'), float('-inf'))
+    Traceback (most recent call last):
+    	      ...
+    OverflowError: cannot convert float infinity to integer
+
+Case NaN:
+
+    >>> add_integer(0, float('nan'))
+    Traceback (most recent call last):
+    	      ...
+    ValueError: cannot convert float NaN to integer
